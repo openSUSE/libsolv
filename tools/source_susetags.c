a4555e18161e286e12c4892ce1bafb0d7398be13
Michael Schroeder <mls@suse.de>
2007-10-05 17:24:41 +0000

    do not add self-provides to src rpms

--- tools/source_susetags.c
+++ tools/source_susetags.c
@@ -222,7 +222,7 @@ pool_addsource_susetags(Pool *pool, FILE *fp)
 	continue;
       if (!strncmp(line, "=Pkg:", 5) || !strncmp(line, "=Pat:", 5))
 	{
-	  if (dp)
+	  if (dp && s->arch != ARCH_SRC && s->arch != ARCH_NOSRC)
 	    dp->provides = source_addid_dep(source, dp->provides, rel2id(pool, s->name, s->evr, REL_EQ, 1), 0);
 	  pd.kind = 0;
 	  if (line[3] == 't')
