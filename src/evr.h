61959ce64d745d8663edfabbcfb74c4b90e5c107
Michael Matz <matz@suse.de>
2007-10-20 16:30:36 +0000

    I need C++ here too.

--- src/evr.h
+++ src/evr.h
@@ -6,9 +6,17 @@
 #ifndef EVR_H
 #define EVR_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "pooltypes.h"
 
 extern int vercmp( const char *s1, const char *q1, const char *s2, const char *q2 );
 extern int evrcmp( Pool *pool, Id evr1id, Id evr2id );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* EVR_H */
