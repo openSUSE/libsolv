4c8c559dd7f252cb947c3fd80632ef8fe5777a3f
Michael Matz <matz@suse.de>
2008-01-14 16:33:13 +0000

    Let's give fastlz a license, shall we?

--- src/fastlz.c
+++ src/fastlz.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2007, Novell Inc.
+ *
+ * This program is licensed under the BSD license, read LICENSE.BSD
+ * for further information
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
