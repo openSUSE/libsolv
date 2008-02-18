0703c7fe80a50cd1c0bfb18e415c9ff756c3798a
Klaus Kaempf <kkaempf@suse.de>
2008-02-18 15:17:20 +0000

    pass column number to error message

--- tools/repo_rpmmd.c
+++ tools/repo_rpmmd.c
@@ -778,7 +778,7 @@ repo_add_rpmmd(Repo *repo, FILE *fp, int flags)
       l = fread(buf, 1, sizeof(buf), fp);
       if (XML_Parse(parser, buf, l, l == 0) == XML_STATUS_ERROR)
 	{
-	  fprintf(stderr, "repo_rpmmd: %s at line %u\n", XML_ErrorString(XML_GetErrorCode(parser)), (unsigned int)XML_GetCurrentLineNumber(parser));
+	  fprintf(stderr, "repo_rpmmd: %s at line %u:%u\n", XML_ErrorString(XML_GetErrorCode(parser)), (unsigned int)XML_GetCurrentLineNumber(parser), (unsigned int)XML_GetCurrentColumnNumber(parser));
 	  exit(1);
 	}
       if (l == 0)
