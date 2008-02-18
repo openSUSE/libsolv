260c774e9842a56eebaadd7ad63a13099eca21e0
Klaus Kaempf <kkaempf@suse.de>
2008-02-18 09:44:34 +0000

    add column to error output

--- tools/repo_patchxml.c
+++ tools/repo_patchxml.c
@@ -678,7 +678,7 @@ repo_add_patchxml(Repo *repo, FILE *fp, int flags)
       l = fread(buf, 1, sizeof(buf), fp);
       if (XML_Parse(parser, buf, l, l == 0) == XML_STATUS_ERROR)
 	{
-	  fprintf(stderr, "repo_patchxml: %s at line %u\n", XML_ErrorString(XML_GetErrorCode(parser)), (unsigned int)XML_GetCurrentLineNumber(parser));
+	  fprintf(stderr, "repo_patchxml: %s at line %u:%u\n", XML_ErrorString(XML_GetErrorCode(parser)), (unsigned int)XML_GetCurrentLineNumber(parser), (unsigned int)XML_GetCurrentColumnNumber(parser));
 	  exit(1);
 	}
       if (l == 0)
