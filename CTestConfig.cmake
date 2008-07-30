4a83c9fc1da6bcdf08076bf6f48e29db68185182
Duncan Mac-Vicar P <dmacvicar@suse.de>
2008-07-30 16:07:11 +0000

    cdash config

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "sat-solver")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "yast.suse.de")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=sat-solver")
+set(CTEST_DROP_SITE_CDASH TRUE)
