e50bc21f2358a4b87ea43d5380e2e057a6ff2dd6
Duncan Mac-Vicar P <dmacvicar@suse.de>
2008-08-13 14:55:52 +0000

    warning

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,6 @@
 set(CTEST_PROJECT_NAME "sat-solver")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
-
+set(UPDATE_TYPE "svn")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "yast.suse.de")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=sat-solver")
