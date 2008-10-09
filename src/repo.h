862fac73a3383ad4032239766d69f3dca3447db4
Michael Schroeder <mls@suse.de>
2008-10-09 13:14:26 +0000

    - declare dataiterator_free - more bindings fixes

--- src/repo.h
+++ src/repo.h
@@ -279,6 +279,7 @@ typedef struct _Dataiterator
        dosomething(di.solvid, di.key, di.kv);  */
 void dataiterator_init(Dataiterator *di, Repo *repo, Id p, Id keyname,
 		       const char *match, int flags);
+void dataiterator_free(Dataiterator *di);
 int dataiterator_step(Dataiterator *di);
 int dataiterator_match(Dataiterator *di, int flags, const void *match);
 void dataiterator_skip_attribute(Dataiterator *di);
