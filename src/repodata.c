7603e49df7cd0d4539f2e07d9b926e67396a878a
Michael Schroeder <mls@suse.de>
2009-03-05 14:29:17 +0100

    - fix repodata_merge_attrs() function

--- src/repodata.c
+++ src/repodata.c
@@ -1955,7 +1955,7 @@ void
 repodata_merge_attrs(Repodata *data, Id dest, Id src)
 {
   Id *keyp;
-  if (dest == src || !(keyp = data->attrs[src]))
+  if (dest == src || !(keyp = data->attrs[src - data->start]))
     return;
   for (; *keyp; keyp += 2)
     repodata_insert_keyid(data, dest, keyp[0], keyp[1], 0);
