99ceec3e3b62689b8de08dcb2a38924687845268
Michael Schroeder <mls@suse.de>
2009-07-13 18:54:48 +0200

    - add rpm_byrpmh and rpm_query functions

--- ext/repo_rpmdb.h
+++ ext/repo_rpmdb.h
@@ -6,6 +6,9 @@
  */
 
 #include "queue.h"
+#include "repo.h"
+
+struct headerToken_s;
 
 extern void repo_add_rpmdb(Repo *repo, Repo *ref, const char *rootdir, int flags);
 extern void repo_add_rpms(Repo *repo, const char **rpms, int nrpms, int flags);
@@ -24,5 +27,9 @@ void repo_add_pubkeys(Repo *repo, const char **keys, int nkeys, int flags);
 
 void *rpm_byrpmdbid(Id rpmdbid, const char *rootdir, void **statep);
 void *rpm_byfp(FILE *fp, const char *name, void **statep);
+void *rpm_byrpmh(struct headerToken_s *h, void **statep);
+
+
+char *rpm_query(void *rpmhandle, Id what);
 void rpm_iterate_filelist(void *rpmhandle, int flags, void (*cb)(void *, const char *, int, const char *), void *cbdata);
 int  rpm_installedrpmdbids(const char *rootdir, Queue *rpmdbidq);
