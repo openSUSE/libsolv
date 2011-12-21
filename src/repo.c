22bfbde6d4c8a71c34b8194fa4e75ccc9bdd7516
Michael Schroeder <mls@suse.de>
2011-12-21 12:28:46 +0100

    - clear lastmarkerpos when recreating the hash

--- src/repo.c
+++ src/repo.c
@@ -330,6 +330,7 @@ repo_addid_dep_hash(Repo *repo, Offset olddeps, Id id, Id marker, int size)
   /* maintain hash and lastmarkerpos */
   if (repo->lastidhash_idarraysize != repo->idarraysize || size * 2 > repo->lastidhash_mask || repo->lastmarker != marker)
     {
+      repo->lastmarkerpos = 0;
       if (size * 2 > repo->lastidhash_mask)
 	{
 	  repo->lastidhash_mask = mkmask(size < REPO_ADDID_DEP_HASHMIN ? REPO_ADDID_DEP_HASHMIN : size);
