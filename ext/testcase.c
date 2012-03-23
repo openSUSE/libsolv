1f79370cd88449514cc784f6a6fb7089edbb5822
Michael Schroeder <mls@suse.de>
2012-03-23 14:41:01 +0100

    - don't call pool_solvid2str in testcase_solvid2str
    That way we can change the pool_solvid2str output without breaking testcases

--- ext/testcase.c
+++ ext/testcase.c
@@ -338,12 +338,16 @@ const char *
 testcase_solvid2str(Pool *pool, Id p)
 {
   Solvable *s = pool->solvables + p;
-  const char *str;
-  char buf[20];
+  const char *n, *e, *a;
+  char *str, buf[20];
 
   if (p == SYSTEMSOLVABLE)
     return "@SYSTEM";
-  str = pool_solvid2str(pool, p);
+  n = pool_id2str(pool, s->name);
+  e = pool_id2str(pool, s->evr);
+  a = pool_id2str(pool, s->arch);
+  str = pool_alloctmpspace(pool, strlen(n) + strlen(e) + strlen(a) + 3); 
+  sprintf(str, "%s-%s.%s", n, e, a); 
   if (!s->repo)
     return pool_tmpappend(pool, str, "@", 0);
   if (s->repo->name)
