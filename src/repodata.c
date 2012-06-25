975ce4f08e536a84cef482f676a86c6d55f1ca92
Michael Schroeder <mls@suse.de>
2012-06-25 13:42:25 +0200

    - fix typo in repodata_merge_attrs

--- src/repodata.c
+++ src/repodata.c
@@ -2425,7 +2425,7 @@ void
 repodata_merge_attrs(Repodata *data, Id dest, Id src)
 {
   Id *keyp;
-  if (dest == src || data->attrs || !(keyp = data->attrs[src - data->start]))
+  if (dest == src || !data->attrs || !(keyp = data->attrs[src - data->start]))
     return;
   for (; *keyp; keyp += 2)
     repodata_insert_keyid(data, dest, keyp[0], keyp[1], 0);
