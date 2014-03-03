openSUSE/libsolv

commit 0804020d92975c20a683f490f81339c14555b2d4

datamatcher_init: play it safe and do a strdup on the match string

src/repodata.c
@@ -1071,7 +1071,7 @@ solvabledata_fetch(Solvable *s, KeyValue *kv, Id keyname)
 int
 datamatcher_init(Datamatcher *ma, const char *match, int flags)
 {
-  ma->match = match;
+  ma->match = match ? solv_strdup(match) : 0;
   ma->flags = flags;
   ma->error = 0;
   ma->matchdata = 0;
@@ -1108,6 +1108,8 @@ datamatcher_init(Datamatcher *ma, const char *match, int flags)
 void
 datamatcher_free(Datamatcher *ma)
 {
+  if (ma->match)
+    ma->match = solv_free((char *)ma->match);
   if ((ma->flags & SEARCH_STRINGMASK) == SEARCH_REGEX && ma->matchdata)
     {
       regfree(ma->matchdata);

commit 33e469790ae08fdf674c878c39bdf83af335607b

fix basename optimization for STRINGEND
broken with commit 0804020d92975c20a683f490f81339c14555b2d4

src/repodata.c
@@ -1091,7 +1091,8 @@ solvabledata_fetch(Solvable *s, KeyValue *kv, Id keyname)
 int
 datamatcher_init(Datamatcher *ma, const char *match, int flags)
 {
-  ma->match = match ? solv_strdup(match) : 0;
+  match = match ? solv_strdup(match) : 0;
+  ma->match = match;
   ma->flags = flags;
   ma->error = 0;
   ma->matchdata = 0;