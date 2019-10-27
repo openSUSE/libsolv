66ca78cfcf3a59b7b22ccd35a975658b56887b91
h0neybadger <h0neyb4dgerdc@gmail.com>
2019-10-27 12:35:40 +0100

    Fix repodata_set_sourcepkg 'SOLVABLE_SOURCENAME' and 'SOLVABLE_SOURCEEVR'
    Remove strncmp check to allow solvable with a different source 'name'

--- src/repodata.c
+++ src/repodata.c
@@ -2817,18 +2817,10 @@ repodata_set_sourcepkg(Repodata *data, Id solvid, const char *sourcepkg)
     return;
   sevr = p + 1;
   pool = s->repo->pool;
-
-  name = pool_id2str(pool, s->name);
-  if (name && !strncmp(sourcepkg, name, sevr - sourcepkg - 1) && name[sevr - sourcepkg - 1] == 0)
-    repodata_set_void(data, solvid, SOLVABLE_SOURCENAME);
-  else
-    repodata_set_id(data, solvid, SOLVABLE_SOURCENAME, pool_strn2id(pool, sourcepkg, sevr - sourcepkg - 1, 1));
-
-  evr = evrid2vrstr(pool, s->evr);
-  if (evr && !strncmp(sevr, evr, sarch - sevr - 1) && evr[sarch - sevr - 1] == 0)
-    repodata_set_void(data, solvid, SOLVABLE_SOURCEEVR);
-  else
-    repodata_set_id(data, solvid, SOLVABLE_SOURCEEVR, pool_strn2id(pool, sevr, sarch - sevr - 1, 1));
+  
+  // most of the time the source name does not match the package name 
+  repodata_set_id(data, solvid, SOLVABLE_SOURCENAME, pool_strn2id(pool, sourcepkg, sevr - sourcepkg - 1, 1));
+  repodata_set_id(data, solvid, SOLVABLE_SOURCEEVR, pool_strn2id(pool, sevr, sarch - sevr - 1, 1));
 
   if (!strcmp(sarch, "src.rpm"))
     repodata_set_constantid(data, solvid, SOLVABLE_SOURCEARCH, ARCH_SRC);
