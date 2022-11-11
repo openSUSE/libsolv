openSUSE/libsolv

commit 26fcfa714a29d1feb393e9a61f9d1a04556866a6

Prefer SOLVER_RULE_PKG_SAME_NAME in solver_ruleinfo

src/rules.c
@@ -3008,7 +3008,7 @@ solver_ruleinfo(Solver *solv, Id rid, Id *fromp, Id *top, Id *depp)
 	  qp = rq.elements[i + 1];
 	  qo = rq.elements[i + 2];
 	  qd = rq.elements[i + 3];
-	  if (type == SOLVER_RULE_PKG || type > qt)
+	  if (type == SOLVER_RULE_PKG || SOLVER_RULE_PKG_SAME_NAME || type > qt)
 	    {
 	      type = qt;
 	      if (fromp)
@@ -3017,6 +3017,8 @@ solver_ruleinfo(Solver *solv, Id rid, Id *fromp, Id *top, Id *depp)
 		*top = qo;
 	      if (depp)
 		*depp = qd;
+	      if (qt == SOLVER_RULE_PKG_SAME_NAME)
+		break;			/* prefer SOLVER_RULE_PKG_SAME_NAME */
 	    }
 	}
       queue_free(&rq);

commit fc10f1d7b76c6650188a85f17ede3355ee4b915e

Fix type check broken by commit 26fcfa714a29d1feb393e9a61f9d1a04556866a6
Caught by our libzypp test cases. I should run them more often.

src/rules.c
@@ -3008,7 +3008,7 @@ solver_ruleinfo(Solver *solv, Id rid, Id *fromp, Id *top, Id *depp)
 	  qp = rq.elements[i + 1];
 	  qo = rq.elements[i + 2];
 	  qd = rq.elements[i + 3];
-	  if (type == SOLVER_RULE_PKG || SOLVER_RULE_PKG_SAME_NAME || type > qt)
+	  if (type == SOLVER_RULE_PKG || qt == SOLVER_RULE_PKG_SAME_NAME || type > qt)
 	    {
 	      type = qt;
 	      if (fromp)