031a6a7590106d2dddd1504b2b9c44f8a63df5a9
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-02-15 12:08:49 +0100

    fix: header extern C and guard

--- src/conda.h
+++ src/conda.h
@@ -13,10 +13,21 @@
 #ifndef LIBSOLV_CONDA_H
 #define LIBSOLV_CONDA_H
 
+#include "pooltypes.h"
+#include "solvable.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int pool_evrcmp_conda(const Pool *pool, const char *evr1, const char *evr2, int mode);
 int solvable_conda_matchversion(Solvable *s, const char *version);
 Id pool_addrelproviders_conda(Pool *pool, Id name, Id evr, Queue *plist);
 Id pool_conda_matchspec(Pool *pool, const char *name);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LIBSOLV_CONDA_H */
 
