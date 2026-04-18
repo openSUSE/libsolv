847aa28b4bfa6af4f196f4a509864880d1eb0f59
Maat <434725+pvi-github@users.noreply.github.com>
2026-04-18 18:58:06 +0200

    repo_mdk: fix @recommends@ tag offset
    Fix the offset to buf + 12 to match the tag length, aligning with every other branch in the dispatcher.

--- ext/repo_mdk.c
+++ ext/repo_mdk.c
@@ -124,7 +124,7 @@ repo_add_mdk(Repo *repo, FILE *fp, int flags)
       else if (!strncmp(buf + 1, "requires@", 9))
 	s->requires = parse_deps(s, buf + 10, SOLVABLE_PREREQMARKER);
       else if (!strncmp(buf + 1, "recommends@", 11))
-	s->recommends = parse_deps(s, buf + 10, 0);
+	s->recommends = parse_deps(s, buf + 12, 0);
       else if (!strncmp(buf + 1, "suggests@", 9))
 	s->suggests = parse_deps(s, buf + 10, 0);
       else if (!strncmp(buf + 1, "obsoletes@", 10))
